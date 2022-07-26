71b23e9acb8efce53049fbc0af49955d33d00def
najmibrother1 <89541004+najmibrother1@users.noreply.github.com>
2022-07-27 03:01:50 +0500

    Update [letter].js

--- pages/stores/[letter].js
+++ pages/stores/[letter].js
@@ -171,7 +171,7 @@ const Store = ({ brands }) => {
                       <div key={item?._id} className='md:col-span-4'>
                         {item?.merchantname && (
                           <div className='flex items-center'>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                               <path fillrule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
                             </svg>
                             <Link href={`/coupons/${item?.slug}`}>
